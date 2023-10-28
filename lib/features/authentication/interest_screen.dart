4c736892de1fc2725d19f9170e5803f05bacd3d2
greeenCode <ie-woo@hanmail.net>
2023-10-28 14:15:15 +0900

    #5.3 Tutorial Screen

--- lib/features/authentication/interest_screen.dart
+++ lib/features/authentication/interest_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/onboarding/interest_button.dart';
+import 'package:tiktok_clone/features/onboarding/tutorial_screen.dart';
 
 const List interests = [
   "Daily Life",
@@ -66,6 +67,14 @@ class _InterestScreenState extends State<InterestScreen> {
         _showTitle = false;
       });
     }
+  }
+
+  void _onNextTap() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const TutorialScreen(),
+        ));
   }
 
   @override
@@ -141,20 +150,23 @@ class _InterestScreenState extends State<InterestScreen> {
             left: Sizes.size24,
             right: Sizes.size24,
           ),
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: Sizes.size16 + Sizes.size2,
-            ),
-            decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-            ),
-            child: const Text(
-              "Next",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: Sizes.size16,
-                fontWeight: FontWeight.bold,
+          child: GestureDetector(
+            onTap: _onNextTap,
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: Sizes.size16 + Sizes.size2,
+              ),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+              child: const Text(
+                "Next",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
