652c1737026ab795944d0731940c2b72add0d8fa
greeenCode <ie-woo@hanmail.net>
2023-10-29 17:05:55 +0900

    #6.2 BottomNavigationBar

--- lib/features/main_navigtion/main_navigation_screen.dart
+++ lib/features/main_navigtion/main_navigation_screen.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final screen = [
+    const Center(
+      child: Text("Home"),
+    ),
+    const Center(
+      child: Text("Search"),
+    ),
+    const Center(
+      child: Text("Home"),
+    ),
+    const Center(
+      child: Text("Search"),
+    ),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: screen[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.shifting,
+          currentIndex: _selectedIndex,
+          onTap: _onTap,
+          items: const [
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.house),
+              label: "Home",
+              backgroundColor: Colors.amber,
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+              label: "Search",
+              backgroundColor: Colors.blueAccent,
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.house),
+              label: "Home",
+              backgroundColor: Colors.red,
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+              label: "Search",
+              backgroundColor: Colors.teal,
+            ),
+          ],
+        ));
   }
 }
