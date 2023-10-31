01666503b932dcd94e100b1d247b1fff48942fc5
greeenCode <ie-woo@hanmail.net>
2023-10-31 20:37:31 +0900

    #6.5 Custom NavigationBar

--- lib/features/main_navigtion/main_navigation_screen.dart
+++ lib/features/main_navigtion/main_navigation_screen.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/main_navigtion/widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -35,20 +36,42 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(items: const [
-        BottomNavigationBarItem(
-          icon: Icon(
-            CupertinoIcons.house,
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                text: "Home",
+                icon: FontAwesomeIcons.house,
+                isSelected: _selectedIndex == 0,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: "Discover",
+                icon: FontAwesomeIcons.magnifyingGlass,
+                isSelected: _selectedIndex == 1,
+                onTap: () => _onTap(1),
+              ),
+              NavTab(
+                text: "Inbox",
+                icon: FontAwesomeIcons.message,
+                isSelected: _selectedIndex == 3,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: "Profile",
+                icon: FontAwesomeIcons.user,
+                isSelected: _selectedIndex == 4,
+                onTap: () => _onTap(4),
+              ),
+            ],
           ),
-          label: "Home",
         ),
-        BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.search),
-          label: "Search",
-        ),
-      ]),
-      tabBuilder: (context, index) => screen[index],
+      ),
     );
   }
 }
