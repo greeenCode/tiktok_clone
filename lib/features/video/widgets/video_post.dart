5aafa92c4b6e54db847a2ec0d63a0e91ebcd9922
greeenCode <ie-woo@hanmail.net>
2023-11-08 16:40:35 +0900

    #7.3 Video Player

--- lib/features/video/widgets/video_post.dart
+++ lib/features/video/widgets/video_post.dart
@@ -1,15 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoPost extends StatefulWidget {
-  const VideoPost({super.key});
+  final Function onVideoFinilshed;
+
+  const VideoPost({
+    super.key,
+    required this.onVideoFinilshed,
+  });
 
   @override
   State<VideoPost> createState() => _VideoPostState();
 }
 
 class _VideoPostState extends State<VideoPost> {
+  final VideoPlayerController _videoPlayerController =
+      VideoPlayerController.asset("assets/videos/videos01.mp4");
+
+  void _onVideoChange() {
+    if (_videoPlayerController.value.isInitialized) {
+      if (_videoPlayerController.value.duration ==
+          _videoPlayerController.value.position) {
+        widget.onVideoFinilshed();
+      }
+    }
+  }
+
+  void _initVideoPayer() async {
+    await _videoPlayerController.initialize();
+    _videoPlayerController.play();
+    setState(() {});
+    _videoPlayerController.addListener(_onVideoChange);
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initVideoPayer();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: _videoPlayerController.value.isInitialized
+              ? VideoPlayer(_videoPlayerController)
+              : Container(
+                  color: Colors.blue,
+                ),
+        )
+      ],
+    );
   }
 }
