4042d7b055aefd541210bb09dffe2ad5d369e13b
greeenCode <ie-woo@hanmail.net>
2023-11-14 21:52:15 +0900

    #8.1 Comments

--- lib/features/video/widgets/video_comments.dart
+++ lib/features/video/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -35,6 +36,82 @@ class _VideoCommentsState extends State<VideoComments> {
               icon: const FaIcon(FontAwesomeIcons.xmark),
             )
           ],
+        ),
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          separatorBuilder: (context, index) => Gaps.v20,
+          itemCount: 10,
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: 18,
+                child: Text(
+                  "Singing",
+                  style: TextStyle(
+                    fontSize: Sizes.size10,
+                  ),
+                ),
+              ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Singing",
+                      style: TextStyle(
+                        fontSize: Sizes.size14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                    Gaps.v3,
+                    const Text(
+                        "That's not it l've seen the same thing but also in a cave,"),
+                  ],
+                ),
+              ),
+              Gaps.h10,
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.heart,
+                    color: Colors.grey.shade500,
+                    size: Sizes.size20,
+                  ),
+                  Text(
+                    "31.9K",
+                    style: TextStyle(
+                      color: Colors.grey.shade500,
+                      fontSize: Sizes.size14,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.grey.shade500,
+                foregroundColor: Colors.white,
+                child: const Text(
+                  "Singing",
+                  style: TextStyle(
+                    fontSize: Sizes.size10,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
