9dca2eb958e755898c6e741484b067a750c9f5be
greeenCode <ie-woo@hanmail.net>
2023-11-18 16:34:41 +0900

    #9.2 TabBar

---
+++ lib/features/discover/discover_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+final tabs = [
+  "Top",
+  "Users",
+  "Videos",
+  "Sounds",
+  "LIVE",
+  "Shopping",
+  "Brands",
+];
+
+class DiscoverScreen extends StatelessWidget {
+  const DiscoverScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          title: const Text(
+            "Discover",
+          ),
+          bottom: TabBar(
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size16,
+            ),
+            isScrollable: true,
+            splashFactory: NoSplash.splashFactory,
+            indicatorColor: Colors.black,
+            labelStyle: const TextStyle(
+              fontSize: Sizes.size16,
+              fontWeight: FontWeight.w500,
+            ),
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+            tabs: [
+              for (var tab in tabs)
+                Tab(
+                  text: tab,
+                )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            for (var tab in tabs)
+              Center(
+                child: Text(
+                  tab,
+                  style: const TextStyle(
+                    fontSize: Sizes.size36,
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
